9549a17924efb26a708890be20adfe931adf64ee
adamozsuer <adam2019.ca@gmail.com>
2023-02-18 04:54:54 -0800

    feb18

--- LWC/force-app/main/default/lwc/lwc3/lwc3.js
+++ LWC/force-app/main/default/lwc/lwc3/lwc3.js
@@ -1,3 +1,11 @@
 import { LightningElement } from 'lwc';
 
-export default class Lwc3 extends LightningElement {}
+export default class Lwc3 extends LightningElement {
+textValue;
+    handleInputChange(event) {
+        this.textValue = event.detail.value;
+    }
+
+
+
+}
