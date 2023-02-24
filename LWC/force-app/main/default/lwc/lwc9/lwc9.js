cc320c9a5041fe58e34f8dd3b8b5dee15ea9f78a
adamozsuer <adam2019.ca@gmail.com>
2023-02-24 09:11:51 -0800

    feb24

---
+++ LWC/force-app/main/default/lwc/lwc9/lwc9.js
@@ -0,0 +1,11 @@
+import { LightningElement, api } from 'lwc';
+
+export default class Lwc9 extends LightningElement {
+ 
+       
+    
+        // Flexipage provides recordId and objectApiName
+        @api recordId;
+        @api objectApiName;
+
+}
