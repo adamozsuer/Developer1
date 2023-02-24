3076ae2b92765094962e3018c7323eb4f2dfd591
adamozsuer <adam2019.ca@gmail.com>
2023-02-24 07:42:12 -0800

    feb2423

--- LWC/force-app/main/default/lwc/lwc7/lwc7.js
+++ LWC/force-app/main/default/lwc/lwc7/lwc7.js
@@ -1,3 +1,33 @@
 import { LightningElement } from 'lwc';
 
-export default class Lwc7 extends LightningElement {}
+export default class Lwc7 extends LightningElement {
+
+contactList = [
+
+{
+Id: 1,
+Name: "John Doe",
+Title: "CEO"
+
+},
+{
+Id: 2,
+Name: "Jennifer Smith",
+Title: "Senior VP"
+
+
+},
+
+{
+    Id: 3,
+    Name: "Daniel Moon",
+    Title: "VP of Business Operations"
+    
+    
+    }
+]
+
+
+
+
+}
