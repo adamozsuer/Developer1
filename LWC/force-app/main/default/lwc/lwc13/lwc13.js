00b28a311cf5822de14f4c52fe227ec0249ce179
adamozsuer <adam2019.ca@gmail.com>
2023-02-24 18:21:24 -0800

    feb24

--- LWC/force-app/main/default/lwc/lwc13/lwc13.js
+++ LWC/force-app/main/default/lwc/lwc13/lwc13.js
@@ -3,7 +3,7 @@ import { LightningElement, api } from 'lwc';
 
 export default class Lwc13 extends LightningElement {
 
-    @api richtext = '<h2>Default <s>Value</s></h2>';
+    @api richtext;
 @api richtextLabel;
 
 
